20d7c6e043fbe38e51a93b0070cf9376061da3fa
Tony <tonyw@cs.utexas.edu>
2013-10-04 17:08:52 -0500

    LED blinks blue and red

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -1,14 +1,31 @@
 #include <RASLib/inc/common.h>
+#include <RASLib/inc/gpio.h>
+#include <RASLib/inc/time.h>
+
+#define RED 0
+#define BLUE 1
+
+void toggleLed(int* currColor) {
+	switch(*currColor) {
+		// PIN_GREEN is actually blue.
+		case RED:
+			SetPin(PIN_GREEN, 1);
+			SetPin(PIN_RED, 0);
+			*currColor = BLUE;
+			break;
+		case BLUE:
+			SetPin(PIN_GREEN, 0);
+			SetPin(PIN_RED, 1);
+			*currColor = RED;
+			break;
+	}
+}
 
 // The 'main' function is the entry point of the program
 int main(void) {
-    InitializeMCU();
-    
-    // Initialization code can go here
-    
-    while (1) {
-        
-        // Runtime code can go here
-        
-    }
+	int currColor = 0;
+  InitializeMCU();
+  CallEvery(toggleLed, &currColor, 1.0f);
+	while (1) {
+	}
 }
