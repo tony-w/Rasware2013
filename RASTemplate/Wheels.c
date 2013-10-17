2e00a23a3ab500f220d71f07bc4f7cf54eb9642c
Tony <tonyw@cs.utexas.edu>
2013-10-16 22:13:10 -0500

    Automatically initialize wheels in drive() if not already initialized

--- RASTemplate/Wheels.c
+++ RASTemplate/Wheels.c
@@ -3,14 +3,19 @@
 #include "Wheels.h"
 
 tMotor *motors[12];
+tBoolean wheelsInitialized = false;
 
 void initWheels(void) {
 	motors[LEFT_WHEEL_MOTOR] = InitializeMotor(LEFT_PIN1, LEFT_PIN2, true, false);
   motors[RIGHT_WHEEL_MOTOR] = InitializeMotor(RIGHT_PIN1, RIGHT_PIN2, true, false);
+	wheelsInitialized = true;
 }
 
 void drive(int direction) {
 	float leftSpeed = STOP, rightSpeed = STOP;
+	if(!wheelsInitialized) {
+		initWheels();
+	}
 	switch(direction) {
 		case FORWARD:
 			rightSpeed = SPEED;
