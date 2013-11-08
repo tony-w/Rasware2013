ac7c883a3d357a27ec62aa2093a5a5b399fddde5
Tony <tonyw@cs.utexas.edu>
2013-11-08 01:26:20 -0600

    Removed duplicate defines from headers

--- RASTemplate/LineFollow.h
+++ RASTemplate/LineFollow.h
@@ -1,11 +1,6 @@
 #ifndef _LINE_FOLLOW_H_
 #define _LINE_FOLLOW_H_
 
-#define LINE_SENSOR_SDA_PIN PIN_B3
-#define LINE_SENSOR_SCL_PIN PIN_B2
-
-#define LINE_SENSOR_THRESHOLD 1.5f
-
 void followLine(void);
 
 #endif
--- RASTemplate/WallFollow.h
+++ RASTemplate/WallFollow.h
@@ -1,10 +1,6 @@
 #ifndef _WALL_FOLLOW_H_
 #define _WALL_FOLLOW_H_
 
-#define IR_SENSOR_0_PIN PIN_D0
-
-#define NUM_ADC_VALUES 5
-
 #define ADC_RIGHT_THRESHOLD 0.6f
 #define ADC_LEFT_THRESHOLD 0.4f
 
