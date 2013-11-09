c5a355c9c6ab8145068cda9c7c9864f625e00c95
Tony <tonyw@cs.utexas.edu>
2013-11-09 17:02:22 -0600

    Renamed lastDirection to direction (for real)

--- RASTemplate/LineFollow.c
+++ RASTemplate/LineFollow.c
@@ -2,14 +2,15 @@
 #include "LineSensor.h"
 #include "Wheels.h"
 
+
 void followLine(void) {
-	int lastDirection = FORWARD;
+	int direction = FORWARD;
 	if((isSensingLineOnLeft() && isSensingLineOnRight()) || isSensingLineCenter()) {
-		lastDirection = FORWARD;
+		direction = FORWARD;
 	} else if(isSensingLineOnLeft()) {
-		lastDirection = LEFT_IN_PLACE;
+		direction = LEFT_IN_PLACE;
 	} else if(isSensingLineOnRight()) {
-		lastDirection = RIGHT_IN_PLACE;
-	}
-	drive(lastDirection);
+		direction = RIGHT_IN_PLACE;
+	} 
+	drive(direction);
 }
